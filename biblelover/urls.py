a2d18ad8085084a8debaad32eeb35c60c7006a7f
Taiwo Kareem <taiwo.kareem36@gmail.com>
2020-04-05 11:04:43 +0100

    removed circleci

--- biblelover/urls.py
+++ biblelover/urls.py
@@ -11,5 +11,4 @@ urlpatterns = [
             {'document_root': settings.STATIC_ROOT}),
 ]
 
-if settings.ENV != "CI":
-    delete_last_verse_of_the_day(verbose_name="Delete last verse of the day", repeat=300)
+delete_last_verse_of_the_day(verbose_name="Delete last verse of the day", repeat=300)
