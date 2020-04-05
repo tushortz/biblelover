b10affdc7ae2afdf614ead17ca6b16b595c10618
Taiwo Kareem <taiwo.kareem36@gmail.com>
2020-04-05 05:34:36 +0100

    yes

--- biblelover/settings.py
+++ biblelover/settings.py
@@ -16,7 +16,7 @@ import dj_database_url
 
 def get_env_variable(var_name, default=None):
     if default == None:
-        return ""
+        return var_name
 
     """Get environment variable by name."""
     try:
@@ -33,7 +33,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production secret! 
 SECRET_KEY = get_env_variable('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
