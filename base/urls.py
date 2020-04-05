061ab147b5f174fb6ced5009474cd325dd667bcb
Taiwo Kareem <taiwo.kareem36@gmail.com>
2020-04-05 03:27:33 +0100

    update file

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, re_path
 from base import views as base_view
+from bible.tasks import delete_last_verse_of_the_day
 
 
 app_name = "base"
@@ -7,3 +8,5 @@ app_name = "base"
 urlpatterns = [
     path("", view=base_view.index, name="index")
 ]
+
+delete_last_verse_of_the_day(verbose_name="Delete last verse of the day", repeat=300)
--- base/views.py
+++ base/views.py
@@ -1,16 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from bible.helpers import verse_of_the_day_helper
-from bible.tasks import delete_last_verse_of_the_day
+
 
 # @login_required
 def index(request):
+
     context = {
         "verse_of_the_day": verse_of_the_day_helper.get_verse_of_the_day()
     }
 
     return render(request, "base/index.html", context)
-
-
-
-delete_last_verse_of_the_day(verbose_name="Delete last verse of the day", repeat=300)
