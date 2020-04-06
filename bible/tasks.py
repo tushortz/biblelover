cce2e321db26b522f980e226874b7e815da198b4
Taiwo Kareem <taiwo.kareem36@gmail.com>
2020-04-06 18:50:37 +0100

    fixed task daily run for verse of the day widget

--- bible/tasks.py
+++ bible/tasks.py
@@ -1,10 +1,11 @@
 from background_task import background
 from bible.models import VerseOfTheDay
 
-@background(schedule=60 * 60 * 24)
+DAILY = 60 * 60 * 24
+@background(schedule=DAILY)
 def delete_last_verse_of_the_day():
     if VerseOfTheDay.objects.count() > 0:
         VerseOfTheDay.objects.first().delete()
 
 
-delete_last_verse_of_the_day(verbose_name="Delete last verse of the day", repeat=300)
+delete_last_verse_of_the_day(verbose_name="Delete last verse of the day", repeat=DAILY)
