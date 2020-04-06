6de78898c980370734b5e0ee7ed956fb83cd33f3
Taiwo Kareem <taiwo.kareem36@gmail.com>
2020-04-06 09:23:37 +0100

    fixed backgroundtask

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, re_path
 from base import views as base_view
-from bible.tasks import delete_last_verse_of_the_day
 
 
 app_name = "base"
--- bible/tasks.py
+++ bible/tasks.py
@@ -5,3 +5,6 @@ from bible.models import VerseOfTheDay
 def delete_last_verse_of_the_day():
     if VerseOfTheDay.objects.count() > 0:
         VerseOfTheDay.objects.first().delete()
+
+
+delete_last_verse_of_the_day(verbose_name="Delete last verse of the day", repeat=300)
