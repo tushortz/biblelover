1a045738d74329eaad4f5ba67723122536f4b3e7
Taiwo Kareem <taiwo.kareem36@gmail.com>
2020-04-06 10:47:55 +0100

    this is to test danger files

--- bible/tasks.py
+++ bible/tasks.py
@@ -1,10 +1,12 @@
 from background_task import background
 from bible.models import VerseOfTheDay
 
+
 @background(schedule=60 * 60 * 24)
 def delete_last_verse_of_the_day():
     if VerseOfTheDay.objects.count() > 0:
         VerseOfTheDay.objects.first().delete()
 
 
-delete_last_verse_of_the_day(verbose_name="Delete last verse of the day", repeat=300)
+delete_last_verse_of_the_day(
+    verbose_name="Delete last verse of the day", repeat=300)
--- dangerfile.py
+++ dangerfile.py
@@ -2,4 +2,5 @@ from danger_python import Danger, markdown
 
 danger = Danger()
 title = danger.github.pr.title
+danger.github.commits
 markdown(title)
