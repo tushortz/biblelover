f978eaf281061128e47c0ae008dec8a870c422d0
Taiwo Kareem <taiwo.kareem36@gmail.com>
2020-04-07 08:48:07 +0100

    updated settings file

--- biblelover/settings/development.py
+++ biblelover/settings/development.py
@@ -3,7 +3,7 @@ import os
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DEBUG = False
+DEBUG = True
 
 SECRET_KEY = "g%*g5pj-exx+4ad9092*st_@fttv!rl37m(m@32p622bg3h8p+"
 
