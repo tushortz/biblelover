e35fbb8e570aec3e224b8229cc2df9ab9da47a2d
Taiwo Kareem <taiwo.kareem36@gmail.com>
2020-04-07 11:35:44 +0100

    updated code

--- biblelover/settings/production.py
+++ biblelover/settings/production.py
@@ -9,7 +9,7 @@ DATABASES = {}
 DATABASES['default'] = dj_database_url.parse(
     get_env_variable('DATABASE_URL'), conn_max_age=600)
 
-admin_emails = [get_env_variable("ADMIN_FULL_NAME"), get_env_variable("ADMIN_EMAIL_ADDRESS")]
+admin_emails = [[get_env_variable("ADMIN_FULL_NAME"), get_env_variable("ADMIN_EMAIL_ADDRESS")]]
 
 ADMINS = admin_emails
 
