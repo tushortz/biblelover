e9c477cb2cdad408106e7dd9033917e0b2b47d35
Taiwo Kareem <taiwo.kareem36@gmail.com>
2020-04-08 22:57:59 +0100

    added print function to track when code is run

--- bible/tasks.py
+++ bible/tasks.py
@@ -1,12 +1,15 @@
 from background_task import background
 from bible.models import VerseOfTheDay
+from datetime import datetime
 
 DAILY = 60 * 60 * 24
+
 
 @background(schedule=DAILY)
 def delete_last_verse_of_the_day():
     if VerseOfTheDay.objects.count() > 0:
         VerseOfTheDay.objects.first().delete()
+        print(f"Verse of the day updated at {datetime.now()}")
 
 
 delete_last_verse_of_the_day(
