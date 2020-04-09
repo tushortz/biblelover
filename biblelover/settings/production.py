31047145c5e9810e069cc32a71709dfef1e3d896
Taiwo Kareem <taiwo.kareem36@gmail.com>
2020-04-09 08:32:45 +0100

    swapped static root directory

--- biblelover/settings/production.py
+++ biblelover/settings/production.py
@@ -17,10 +17,10 @@ ADMINS = ADMIN_EMAILS
 
 MANAGERS = ADMIN_EMAILS
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'assets')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 STATICFILES_DIRS = [
-    os.path.join(os.path.dirname(BASE_DIR), 'assets')
+    os.path.join(BASE_DIR, 'assets')
 ]
