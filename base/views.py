65e41cc82b5913431e4a06c74fb5fb3a9343c174
wpena <officialpena@gmail.com>
2020-04-09 22:12:45 +0100

    Created T&C Page and linked it to footer

--- base/views.py
+++ base/views.py
@@ -10,6 +10,9 @@ def index(request):
 
     return render(request, "base/index.html", context)
 
+def terms(request):
+    return render(request, "policy/terms.html")
+
 def dashboard(request):
     context = {
         "verse_of_the_day": verse_of_the_day_helper.get_verse_of_the_day()
