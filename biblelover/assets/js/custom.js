13bdabedf24a798556e3b1a71f8972e8912ae8e5
Taiwo Kareem <taiwo.kareem36@gmail.com>
2020-04-10 00:23:47 +0100

    linked the text to page. also fixed terms checkbox not disabling register button

--- biblelover/assets/js/custom.js
+++ biblelover/assets/js/custom.js
@@ -1,2 +1,6 @@
 $(document).ready(function(){
+  $("input#check-terms").change(function(){
+    // On register page, disable/enable button depending on whether terms checkbox is checked
+    $("input#signup-btn").attr('disabled', !$(this).prop("checked"));
+  })
 });
