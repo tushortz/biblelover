9edd3a8b0f971602af6672f04ed1041d09fa24aa
Taiwo Kareem <taiwo.kareem36@gmail.com>
2020-04-10 15:49:37 +0100

    format document

--- base/views.py
+++ base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib.auth.decorators import login_required
 from bible.helpers import verse_of_the_day_helper
 
+
 def index(request):
 
     context = {
@@ -10,15 +11,17 @@ def index(request):
 
     return render(request, "base/index.html", context)
 
+
 def terms(request):
     return render(request, "policy/terms.html")
+
 
 def privacy(request):
     return render(request, "policy/privacy.html")
 
+
 def dashboard(request):
     context = {
-        "verse_of_the_day": verse_of_the_day_helper.get_verse_of_the_day()
     }
 
     return render(request, "base/dashboard.html", context)
