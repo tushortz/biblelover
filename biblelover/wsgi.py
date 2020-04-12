029ad58bfd47c833436d74723fa49cc6467c05a1
Taiwo Kareem <taiwo.kareem36@gmail.com>
2020-04-12 08:11:57 +0100

    update

--- biblelover/wsgi.py
+++ biblelover/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'biblelover.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'biblelover.settings.production')
 
 application = get_wsgi_application()
